b0ef456622b0f1b9261c1493eab5be188f683cd5
padam <neyas2neya@gmail.com>
2026-07-13 16:33:12 +0900

    fix: fit project screen migration audit id

--- apps/carbonet-api/src/main/resources/db/migration/postgresql/V20260713131000__register_hidden_emission_project_create_screen.sql
+++ apps/carbonet-api/src/main/resources/db/migration/postgresql/V20260713131000__register_hidden_emission_project_create_screen.sql
@@ -8,5 +8,5 @@ VALUES ('H102010201', 10210201, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
 ON CONFLICT (menu_code) DO UPDATE SET sort_ordr=EXCLUDED.sort_ordr, last_updt_pnttm=CURRENT_TIMESTAMP;
 
 INSERT INTO comtccmmndetailcode (code_id, code, code_nm, code_dc, use_at, frst_regist_pnttm, last_updt_pnttm, last_updusr_id)
-VALUES ('HMENU1', 'H102010201', '새 프로젝트 등록', '/emission/project/create', 'Y', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'PROJECT_SCREEN_REGISTRY')
-ON CONFLICT (code_id, code) DO UPDATE SET code_nm=EXCLUDED.code_nm, code_dc=EXCLUDED.code_dc, use_at='Y', last_updt_pnttm=CURRENT_TIMESTAMP, last_updusr_id='PROJECT_SCREEN_REGISTRY';
+VALUES ('HMENU1', 'H102010201', '새 프로젝트 등록', '/emission/project/create', 'Y', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'PROJECT_SCREEN')
+ON CONFLICT (code_id, code) DO UPDATE SET code_nm=EXCLUDED.code_nm, code_dc=EXCLUDED.code_dc, use_at='Y', last_updt_pnttm=CURRENT_TIMESTAMP, last_updusr_id='PROJECT_SCREEN';
