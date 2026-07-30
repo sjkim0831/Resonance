d3d40f4806b62f8288e4fabcbfcac48c137f362c
padam <neyas2neya@gmail.com>
2026-07-31 00:15:01 +0900

    test(deploy): remeasure optimized Java path

--- apps/carbonet-api/src/main/java/egovframework/com/config/DeploymentCanary.java
+++ apps/carbonet-api/src/main/java/egovframework/com/config/DeploymentCanary.java
@@ -12,8 +12,8 @@ import java.util.List;
  */
 public final class DeploymentCanary {
 
-    public static final String CONTRACT_VERSION = "1.0.0";
-    public static final String REVISION = "2026-07-31T00:09:00+09:00";
+    public static final String CONTRACT_VERSION = "1.1.0";
+    public static final String REVISION = "2026-07-31T00:15:00+09:00";
     public static final List<String> REQUIRED_CHECKS = List.of(
             "incremental-java-compilation",
             "immutable-image",
