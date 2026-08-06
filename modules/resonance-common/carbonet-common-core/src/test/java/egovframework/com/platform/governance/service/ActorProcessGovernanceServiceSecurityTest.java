8c6b2c1f251e914a47b7661dc9d1fe0c6cde68b1
padam <neyas2neya@gmail.com>
2026-08-07 07:04:39 +0900

    test(governance): make SQL matchers null safe

--- modules/resonance-common/carbonet-common-core/src/test/java/egovframework/com/platform/governance/service/ActorProcessGovernanceServiceSecurityTest.java
+++ modules/resonance-common/carbonet-common-core/src/test/java/egovframework/com/platform/governance/service/ActorProcessGovernanceServiceSecurityTest.java
@@ -531,9 +531,9 @@ class ActorProcessGovernanceServiceSecurityTest {
 
     @Test
     void activeCompanyManagerActorCanAssignWithoutAPlatformOrBootstrapAdminRole() {
-        when(jdbc.queryForObject(argThat(sql -> sql.contains("actor_code='COMPANY_MANAGER'")&&sql.contains("data_scope='*'")),
+        when(jdbc.queryForObject(argThat(sql -> sql!=null&&sql.contains("actor_code='COMPANY_MANAGER'")&&sql.contains("data_scope='*'")),
                 org.mockito.ArgumentMatchers.eq(Integer.class),any(Object[].class))).thenReturn(1);
-        when(jdbc.queryForObject(argThat(sql -> sql.contains("from comtnemplyrinfo")&&sql.contains("comtnentrprsmber")),
+        when(jdbc.queryForObject(argThat(sql -> sql!=null&&sql.contains("from comtnemplyrinfo")&&sql.contains("comtnentrprsmber")),
                 org.mockito.ArgumentMatchers.eq(Integer.class),any(Object[].class))).thenReturn(1);
 
         service.assignActorAuthorized(Map.of(
@@ -545,9 +545,9 @@ class ActorProcessGovernanceServiceSecurityTest {
 
     @Test
     void sameTenantCompanyManagerRetainsAValidProjectSpecificAssignmentPath() {
-        when(jdbc.queryForObject(argThat(sql -> sql.contains("from emission_project_registry")&&sql.contains("project_id=?")&&sql.contains("tenant_id=?")),
+        when(jdbc.queryForObject(argThat(sql -> sql!=null&&sql.contains("from emission_project_registry")&&sql.contains("project_id=?")&&sql.contains("tenant_id=?")),
                 org.mockito.ArgumentMatchers.eq(Integer.class),any(Object[].class))).thenReturn(1);
-        when(jdbc.queryForObject(argThat(sql -> sql.contains("from comtnemplyrinfo")&&sql.contains("comtnentrprsmber")),
+        when(jdbc.queryForObject(argThat(sql -> sql!=null&&sql.contains("from comtnemplyrinfo")&&sql.contains("comtnentrprsmber")),
                 org.mockito.ArgumentMatchers.eq(Integer.class),any(Object[].class))).thenReturn(1);
 
         service.assignActorAuthorized(Map.of(
